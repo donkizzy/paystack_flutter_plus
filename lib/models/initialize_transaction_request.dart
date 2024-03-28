a6651012f8f3851cbf146fbe8dbf71a803c985e4
Amos Godwin <amosgodwin500@gmail.com>
2024-03-28 23:51:05 +0100

    changed api url

--- lib/models/initialize_transaction_request.dart
+++ lib/models/initialize_transaction_request.dart
@@ -1,20 +1,20 @@
 class InitializeTransactionRequest {
-  final String? email;
-  final String? amount;
+  final String email;
+  final int amount;
   final String? reference;
   final String? currency;
   final String? callbackUrl;
   final String? plan;
   final int? invoiceLimit;
-  final String? channels;
+  final List<String>? channels;
   final String? splitCode;
   final String? subaccount;
   final int? transactionCharge;
   final String? bearer;
 
   InitializeTransactionRequest({
-    this.email,
-    this.amount,
+    required this.email,
+    required this.amount,
     this.reference,
     this.currency,
     this.callbackUrl,
@@ -45,15 +45,15 @@ class InitializeTransactionRequest {
   Map<String, dynamic> toJson() => {
     "email": email,
     "amount": amount,
-    "reference": reference,
-    "currency": currency,
-    "callback_url": callbackUrl,
-    "plan": plan,
-    "invoice_limit": invoiceLimit,
-    "channels": channels,
-    "split_code": splitCode,
-    "subaccount": subaccount,
-    "transaction_charge": transactionCharge,
-    "bearer": bearer,
+   if(reference != null) "reference": reference,
+    if(currency != null)  "currency": currency,
+    if(callbackUrl != null)  "callback_url": callbackUrl,
+    if(plan != null)  "plan": plan,
+    if(invoiceLimit != null)  "invoice_limit": invoiceLimit,
+    if(channels != null)  "channels": channels,
+    if(splitCode != null) "split_code": splitCode,
+    if(subaccount != null) "subaccount": subaccount,
+    if(transactionCharge != null) "transaction_charge": transactionCharge,
+    if(bearer != null)  "bearer": bearer,
   };
 }
